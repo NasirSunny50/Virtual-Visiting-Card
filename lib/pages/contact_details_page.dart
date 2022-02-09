aa0b0e3a423cbde14c74a2657ef46c86d9263bb4
NasirSunny50 <nasir.uddin08@northsouth.edu>
2022-02-09 23:45:27 +0600

    contact details page updated

--- lib/pages/contact_details_page.dart
+++ lib/pages/contact_details_page.dart
@@ -35,11 +35,89 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(_contactModel!.name),
+        backgroundColor: Colors.teal,
       ),
       body: _contactModel == null ? Center(
         child: CircularProgressIndicator(),
       ): ListView(
-        children: [],
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        children: [
+          ListTile(
+            title: Text(_contactModel!.designation!),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(_contactModel!.companyName!),
+                ],
+            ),
+          ),
+          SizedBox(height: 20,),
+          Card(
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    title: Text(_contactModel!.mobile!),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.message_rounded),
+                          onPressed: (){
+
+                          },
+                        ),
+
+                        IconButton(
+                          icon: Icon(Icons.call),
+                          onPressed: (){
+
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  ListTile(
+                    title: Text(_contactModel!.streetAddress!),
+                    trailing: IconButton(
+                      icon: Icon(Icons.map_rounded),
+                      onPressed: (){
+
+                      },
+                    ),
+                  ),
+
+                  ListTile(
+                    title: Text(_contactModel!.email!),
+                    trailing: IconButton(
+                      icon: Icon(Icons.email_rounded),
+                      onPressed: (){
+
+                      },
+                    ),
+                  ),
+
+                  ListTile(
+                    title: Text(_contactModel!.website!),
+                    trailing: IconButton(
+                      icon: Icon(Icons.web_rounded),
+                      onPressed: (){
+
+                      },
+                    ),
+                  ),
+
+
+                ],
+              ),
+            ),
+          )
+        ],
       ) ,
     );
   }
